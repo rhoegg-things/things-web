e7f14332e99b40281ac97b8716461773e164009c
Ryan Hoegg <ryan.hoegg@gmail.com>
2016-01-31 20:26:38 -0600

    Update 2016-02-01T02:26:35.681Z

--- js/application.js
+++ js/application.js
@@ -1,3 +1,10 @@
+$(document).foundation();
+
+var thingsApp = angular.module('thingsApp', ['thingsServices']);
+
+thingsApp.controller('equipmentController', ['$scope', 'Equipment', function($scope, Equipment) {
+    $scope.equipment = Equipment.query();
+}]);
 var thingsServices = angular.module('thingsServices', ['config', 'ngResource']);
 
 thingsServices.factory('Equipment', ['$resource', 'equipmentApi', function($resource, equipmentApi) {
